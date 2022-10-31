4e839b1f82c1bb0fc380b7ea20672a7307935da8
rangga <ranggashadow27@gmail.com>
2022-11-01 01:34:08 +0700

    setting database

---
+++ lib/model/task_model.dart
@@ -0,0 +1,19 @@
+class Task {
+  final int id;
+  final String? task;
+  final DateTime? dateTime;
+
+  Task({
+    this.dateTime,
+    required this.id,
+    this.task,
+  });
+
+  Map<String, dynamic> toMap() {
+    return ({
+      'id': id,
+      'task': task,
+      'creationDate': dateTime.toString(),
+    });
+  }
+}
